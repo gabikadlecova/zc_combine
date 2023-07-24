420c4af44bd440a595fe6384896350333ab5b44e
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2023-07-24 15:55:06 +0200

    Analysis notebooks for every search space (according to the number of convolutions. New fixed scores read directly from naslib. Plot all proxies, also on log axis. Fix nets that have a lot of zeros, aggregate or filter out only those with the same net as modified net.

--- zc_combine/fixes/operations.py
+++ zc_combine/fixes/operations.py
@@ -6,6 +6,20 @@ import pandas as pd
 import seaborn as sns
 
 
+def get_ops_edges_nb201():
+    edge_map = ((1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4))
+    edge_map = {val: i for i, val in enumerate(edge_map)}
+    ops = ['skip_connect', 'none', 'nor_conv_3x3', 'nor_conv_1x1', 'avg_pool_3x3']
+    return ops, edge_map
+
+
+def get_ops_edges_tnb101():
+    edge_map = ((1, 2), (1, 3), (2, 3), (1, 4), (2, 4), (3, 4))
+    edge_map = {val: i for i, val in enumerate(edge_map)}
+    ops = ['none', 'skip_connect', 'nor_conv_3x3', 'nor_conv_1x1', 'avg_pool_3x3']
+    return ops, edge_map
+
+
 def filter_by_range(df, zc, min, max):
     return df[(df[zc] >= min) & (df[zc] <= max)]
 
---
+++ zc_combine/fixes/utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+
+
+def read_new_score(data, new_data, proxy, copy_old_column=False, pad_val=0):
+    data[f'new_{proxy}'] = data[proxy] if copy_old_column else pad_val
+
+    for net_data in new_data:
+        mask = data['net'] == net_data['arch']
+        data_row = data[mask]
+        assert len(data_row) == 1
+        data.loc[mask, f'new_{proxy}'] = net_data[proxy]['score']
+
+
+def aggregate_isomorphic_scores(data, proxy_cols, agg_func=np.median, val_accs_std=3, score_std=3):
+    unique_data = []
+
+    for net in data['new_net'].unique():
+        nets = data[data['new_net'] == net]
+        val_accs = nets['val_accs']
+
+        agg = {s: agg_func(nets[s]) for s in proxy_cols}
+        stds = {f"{s}_std": nets[s].std() for s in proxy_cols}
+        assert len(nets) == 1 or all(s < score_std for s in stds.values())
+        assert len(nets) == 1 or val_accs.std() < val_accs_std, (nets[['net', 'val_accs']], val_accs.std())
+
+        unique_data.append({'net': net, 'val_accs': val_accs.median(), 'val_accs_std': val_accs.std(), **agg, **stds})
+
+    return pd.DataFrame(unique_data)
+
+
+def edge(edge_map, op_list, i, j):
+    return op_list[edge_map[(i, j)]]
+
+
+def zero_outgoing(net, edge_map, zero_op=1):
+    for node in [2, 3]:
+        inactive = all([(edge(edge_map, net, i, node) == zero_op) for i in range(1, node)])
+        if not inactive:
+            continue
+        for j in range(node + 1, 5):
+            net[edge_map[(node, j)]] = zero_op
+    return net
+
+
+def zero_ingoing(net, edge_map, zero_op=1):
+    for node in [3, 2]:
+        inactive = all([(edge(edge_map, net, node, j) == zero_op) for j in range(node + 1, 5)])
+        if not inactive:
+            continue
+        for i in range(1, node):
+            net[edge_map[(i, node)]] = zero_op
+    return net
+
+
+def nb201_default_isomorphic_cell(data, edge_map, zero_op=1):
+    for idx, net in zip(data.index, data['net']):
+        net = [int(i) for i in net.strip('()').split(',')]
+        net1 = zero_outgoing(net, edge_map, zero_op=zero_op)
+        net2 = zero_ingoing(net1, edge_map, zero_op=zero_op)
+
+        net = f"({', '.join([str(i) for i in net2])})"
+        data.loc[idx, 'new_net'] = net
