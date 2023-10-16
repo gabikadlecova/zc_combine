2bb2ca7d40c7e7a51bc9789594a68a5873839af6
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2023-10-16 13:20:00 +0200

    Add runner for training only (without pca).

---
+++ scripts/run_train.sh
@@ -0,0 +1,15 @@
+. "$1"/bin/activate
+
+if [ -n $3 ]; then
+  cache_dir="--cache_dir_ $3"
+fi
+
+if [ -n $4 ]; then
+  version_key="--version_key $4"
+fi
+
+if [ -n $row_id ]; then
+  SUFFIX="-$row_id"
+fi
+
+python train_on_features.py --wandb_key_ "$2" "$train_args" --out_prefix train_"$SUFFIX" "$cache_dir" "$version_key"
