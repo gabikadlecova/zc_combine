5a4e35771976e9b5d478b1badf8f7e4db321d6dd
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2023-11-30 14:10:46 +0100

    Add feature counting channel and strides across positions.

--- zc_combine/features/tnb101_macro.py
+++ zc_combine/features/tnb101_macro.py
@@ -14,6 +14,26 @@ def count_switches(net):
     return op_counts
 
 
+def get_state_at_i(net):
+    channel = 0
+    stride = 0
+
+    res = {}
+    for i in range(6):
+        if i >= len(net):
+            channel = 0
+            stride = 0
+        else:
+            channel += int(net[i]['channel'])
+            stride += int(net[i]['stride'])
+
+        res[f"ch_pos_{i}"] = channel
+        res[f"s_pos_{i}"] = stride
+
+    return res
+
+
 feature_func_dict = {
-    'count_ops': count_switches
+    'count_ops': count_switches,
+    'pos_state': get_state_at_i
 }
