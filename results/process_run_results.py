1779661852619205e7ea35993c2dbdc0c01b77a5
Gabriela Suchopárová <gabi.suchoparova@gmail.com>
2024-01-10 15:04:51 +0100

    Check for duplicated runs in wandb api.

--- results/process_run_results.py
+++ results/process_run_results.py
@@ -21,24 +21,34 @@ DEFAULT_HISTORY = ['tau', 'corr', 'fit_time', 'test_time', '_step']
 @click.option('--metric_keys', default=None)
 @click.option('--out_path', required=True)
 @click.option('--nan_check_path', default=None)
+@click.option('--page_size', default=50, help="Increase only if there is a duplication bug in wandb.")
 @click.option('--history_len', default=50, help="Required length of history (for each run).")
 @click.option('--keep_all_history/--aggregate_history', default=False,
               help="If True, duplicate config rows and keep all metric values from different seeds. Otherwise keep "
                    "only mean and std of a metric (in a single row).")
-def main(key, project, timeout, cfg_args, metric_keys, out_path, nan_check_path, history_len, keep_all_history):
+def main(key, project, timeout, cfg_args, metric_keys, out_path, nan_check_path, page_size, history_len,
+         keep_all_history):
     cfg_args = DEFAULT_CFG_ARGS if cfg_args is None else cfg_args.split(',')
     metric_keys = DEFAULT_HISTORY if metric_keys is None else metric_keys.split(',')
 
     wandb.login(key=key)
 
     api = wandb.Api(timeout=timeout)
-    runs = api.runs(project)  # example project: "USER_NAME" + "/" + project
+    runs = api.runs(project, per_page=page_size)  # example project: "USER_NAME" + "/" + project
 
     # check if all seeds ran for all runs and if no nans are present
     incomplete_runs = []
 
+    visited = {}
+
     results_df = []
     for run in tqdm(runs):
+        if run.id in visited:
+            raise ValueError(f"Run {run.id} is duplicated. Try to increase page size to the number of runs, "
+                             f"or add and remove a run in the project.")
+
+        visited.add(run.id)
+
         row = {}
         for ca in cfg_args:
             if ca not in run.config:
